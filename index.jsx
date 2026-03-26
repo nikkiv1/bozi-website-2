b34d5b2fbea1bedb81883b78ff896548e97d70ac
nikkiv1 <93191441+nikkiv1@users.noreply.github.com>
2026-03-26 18:39:03 +0100

    Create index.jsx with a basic React component

---
+++ index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Index = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>Current Date and Time: 2026-03-26 17:38:31</p>
+    </div>
+  );
+};
+
+export default Index;
